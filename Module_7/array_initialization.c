a6ed4633cf1b6f224905eed86de5fe0939ee0a84
Maisha Osman Umama <umamau418@gmail.com>
2025-10-18 23:15:15 +0600

    add array summation

---
+++ Module_7/array_initialization.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x = 10;
+    int a[5]= {18, 57};
+    for( int i = 0; i<5 ; i++){
+        printf("%d ", a[i]);
+    }
+    printf("%d", x);
+    return 0;
+}
--- Module_7/array_input_output.c
+++ Module_7/array_input_output.c
@@ -11,8 +11,8 @@
 #include<stdio.h>
 int main()
 {
-    int arr[5];
-    for(int i =0; i <5; i++){
+    int arr[500];
+    for(int i =0; i <500; i++){
         scanf("%d",&arr[i]);
     }
     for(int i =0; i <5; i++){
