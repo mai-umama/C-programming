f7cf9efabea506c6ee361f239e6dadba06f42838
Maisha Osman Umama <umamau418@gmail.com>
2025-10-18 12:30:02 +0600

    Add array practices

---
+++ Module_7/array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int arr [5];
+    arr[2]= 40;
+    arr[3]= 30;
+    arr[1]= 10;
+    printf("%d\n", arr[2]);
+    printf("%d\n", arr[3]);
+    printf("%d\n", arr[1]);
+    return 0;
+}
